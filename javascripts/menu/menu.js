a53a155ab6f01cb5a7ac8e7ba6145b7f13470c8b
Vitor Martiniano <vitormwxm@gmail.com>
2026-07-04 01:32:42 -0300

    Feat adjusts on website

--- javascripts/menu/menu.js
+++ javascripts/menu/menu.js
@@ -3,7 +3,14 @@ const menuList = document.querySelector('.menu-list');
 
 menuToggle.addEventListener('click', () => {
     menuList.classList.toggle('active');
-    console.log("Clicou")
     const expanded = menuList.classList.contains('active');
     menuToggle.setAttribute('aria-expanded', expanded);
+});
+
+// Fechar menu ao clicar em um link
+document.querySelectorAll('.menu-list a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuList.classList.remove('active');
+        menuToggle.setAttribute('aria-expanded', 'false');
+    });
 });
